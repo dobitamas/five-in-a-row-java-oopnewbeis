8814495075edaf92a302a7cfa95ba6d8bbbdcbc8
zoltan.sallay <zoltan.sallay@codecool.com>
2019-10-11 18:25:22 +0200

    Add Java starter code

---
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -0,0 +1,10 @@
+package com.codecool.fiveinarow;
+
+public class FiveInARow {
+
+    public static void main(String[] args) {
+        Game game = new Game(11, 11);
+        game.play(5);
+    }
+
+}
