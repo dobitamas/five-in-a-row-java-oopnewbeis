80810f72255fed4e04017045a2695c814c9fbc03
nia <zsofi.deak.nia@gmail.com>
2020-02-10 14:22:52 +0100

    merge

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -3,7 +3,7 @@ package com.codecool.fiveinarow;
 public class FiveInARow {
 
     public static void main(String[] args) {
-        Game game = new Game(5, 5);
+        Game game = new Game(8, 8);
         game.play(5);
     }
 
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -8,6 +8,13 @@ public class Game implements GameInterface {
 
     public Game(int nRows, int nCols) {
         board = new int[nRows][nCols];
+//        The board below is a dummy list for testing
+//        Good to know: 1 = O; 2 = X;
+//        board = new int[][]{
+//                {0,1,0},
+//                {2,0,1},
+//                {0,0,0},
+//        };
         printBoard();
     }
 
@@ -18,6 +25,23 @@ public class Game implements GameInterface {
     public void setBoard(int[][] board) {
 
         this.board = board;
+    }
+
+    public void handleTurn(int howMany){
+        int player1 = 1;
+        int player2 = 2;
+        boolean turn = false;
+        while (!isFull()){
+            if (turn) {
+               getMove(player1);
+               hasWon(player1, howMany);
+               turn = true;
+            } else {
+                getMove(player2);
+                hasWon(player2, howMany);
+                turn = false;
+            }
+        }
     }
 
     public int[] getMove(int player) {
@@ -31,6 +55,11 @@ public class Game implements GameInterface {
     }
 
     public void mark(int player, int row, int col) {
+        if (board[row][col] == 0){
+            board[row][col] = player;
+        } else {
+            System.out.println("This is not a valid coordinate");
+        }
     }
 
     public boolean hasWon(int player, int howMany) {
@@ -43,6 +72,7 @@ public class Game implements GameInterface {
     }
 
     public void printBoard() {
+
         char[] alphabet = " abcdefghijklmnopqrstuvwxyz".toCharArray();
         char[] header = Arrays.copyOfRange(alphabet,0, board.length + 1);
         for (char c : header) {
@@ -52,7 +82,17 @@ public class Game implements GameInterface {
         for (int i =0; i < board.length; i++){
             System.out.print(i+1 + "\t");
             for (int j = 0; j < board[i].length; j++){
-                System.out.print(board[i][j] + "\t");
+                switch (board[i][j]){
+                    case 0:
+                        System.out.print('.' + "\t");
+                        break;
+                    case 1:
+                        System.out.print('O' + "\t");
+                        break;
+                    case 2:
+                        System.out.print('X' + "\t");
+                        break;
+                }
             }
             System.out.println();
         }
