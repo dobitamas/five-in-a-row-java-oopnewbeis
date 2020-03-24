bd0688103361c2be09b6cf18bbecd04080374f65
nagyakornel <nagyakornel@gmail.com>
2020-03-24 14:05:53 +0100

    Implemented isFull function.

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -4,14 +4,7 @@ import java.util.Arrays;
 public class FiveInARow {
 
     public static void main(String[] args) {
-        Game game = new Game(10, 10);
-	int[][] board = game.getBoard();
-	System.out.println(Arrays.deepToString(board));
-        game.play(5);
-        int[] coord = game.getMove(1);
-	game.mark(1, coord[0], coord[1]);
-	int[] coords = game.getMove(2);
-	game.mark(2, coords[0], coords[1]);
+	Game game = new Game(10, 10);	
+	game.play(5);
     }
-
 }
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class Game implements GameInterface {
 
     private int[][] board;
+    private int[] latestMove = new int[2];
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     public Game(int nRows, int nCols) {
         board = new int[nRows][nCols];
@@ -28,9 +30,6 @@ public class Game implements GameInterface {
 		String coordinates = scanner.nextLine();
         	String col = coordinates.substring(0,1).toLowerCase();
 		int row = Integer.parseInt(coordinates.substring(1));
- 
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
 		coord[0] = alphabet.indexOf(col);
 		coord[1] = row - 1;
 		coordExist = isCoordExist(coord);
@@ -38,7 +37,8 @@ public class Game implements GameInterface {
 			coordEmpty = isCoordTaken(coord);
 		}
 	}
-	
+	latestMove[0] = coord[0];
+	latestMove[1] = coord[1];
 	return coord;
     }
 
@@ -48,22 +48,46 @@ public class Game implements GameInterface {
 
     public void mark(int player, int row, int col) {
 	board[row][col] = player;
-	System.out.println(Arrays.deepToString(board));
     }
 
     public boolean hasWon(int player, int howMany) {
-        for(int i = 0; i< board.length; i++){
-        	for( int j = 0;j<board[0].length; j++){
-       
-        	}
-        }
+        System.out.println(Arrays.toString(latestMove));
+	return false;
     }
 
     public boolean isFull() {
-        return false;
+	for (int[] row : board){
+	    for (int cell : row){
+		if (cell == 0){
+		    return false;
+		}
+	    }
+	}
+        return true;
     }
 
     public void printBoard() {
+	System.out.print("  ");
+	for (int i = 1; i < board[0].length; i++){
+	    System.out.print(i + " ");
+	}
+	System.out.println(board[0].length);
+	for (int i = 0; i < board.length; i++){	  
+	    System.out.print(alphabet.substring(i, i + 1).toUpperCase() + " ");
+	    for (int cell : board[i]){
+		switch (cell){
+		    case 1:
+			System.out.print("X ");
+			break;
+		    case 2:
+			System.out.print("O ");
+			break;
+		    default:
+			System.out.print(". ");
+		}
+	    }
+	    System.out.println("");
+	}
     }
 
     public void printResult(int player) {
@@ -73,7 +97,21 @@ public class Game implements GameInterface {
     }
 
     public void play(int howMany) {
-
+	while(true){
+		printBoard();
+		int[] coord = getMove(1);
+		mark(1, coord[0], coord[1]);
+		printBoard();
+		if (hasWon(1, 5)){
+		    break;
+		}	
+		int[] coords = getMove(2);
+		mark(2, coords[0], coords[1]);
+		printBoard();
+		if (hasWon(2, 5)){
+		    break;
+		}
+	}
     }
 	
     public boolean isCoordExist(int[]coord){
@@ -83,20 +121,9 @@ public class Game implements GameInterface {
 		return true;
 	}	
     }
+
     public boolean isCoordTaken(int [] coord){
     	return (board[coord[0]][coord[1]] == 0);
     }
-	
 
 }
-
-
-
-
-
-
-
-
-
-
-
