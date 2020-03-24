094bf5a5335561f18dc70296526324ad75026c6f
dobitamas <dobi.tamas0428@gmail.com>
2020-03-24 11:22:36 +0100

    Validating coordinates of movement

--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,5 +1,6 @@
 package com.codecool.fiveinarow;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Game implements GameInterface {
 
@@ -18,11 +19,28 @@ public class Game implements GameInterface {
     }
 
     public int[] getMove(int player) {
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("Enter coordinates");
-	String coordinates = scanner.nextLine();
-	System.out.println(coordinates);
-        return null;
+    	boolean coordExist = false;
+    	boolean coordEmpty = false;
+    	int[]coord = new int[2];
+    	while( coordExist != true || coordEmpty != true){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter coordinates");
+		String coordinates = scanner.nextLine();
+        	String col = coordinates.substring(0,1).toLowerCase();
+		int row = Integer.parseInt(coordinates.substring(1));
+ 
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+		coord[0] = alphabet.indexOf(col);
+		coord[1] = row - 1;
+		coordExist = isCoordExist(coord);
+		coordEmpty = isCoordTaken(coord);
+	
+	}
+	
+	
+	System.out.println(Arrays.toString(coord));
+	return coord;
     }
 
     public int[] getAiMove(int player) {
@@ -30,6 +48,7 @@ public class Game implements GameInterface {
     }
 
     public void mark(int player, int row, int col) {
+	
     }
 
     public boolean hasWon(int player, int howMany) {
@@ -50,5 +69,30 @@ public class Game implements GameInterface {
     }
 
     public void play(int howMany) {
+
     }
+	
+    public boolean isCoordExist(int[]coord){
+	if(board.length < coord[0] || board[0].length < coord[1]){
+		return false;
+	} else {
+		return true;
+	}	
+    }
+    public boolean isCoordTaken(int [] coord){
+    	return (board[coord[0]][coord[1]] != 0);
+    }
+	
+
 }
+
+
+
+
+
+
+
+
+
+
+
